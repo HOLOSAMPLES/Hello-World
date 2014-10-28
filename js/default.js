43a4183ea4fa2720b7ed64bb6eb190e1649a80c8
liyangv <liyang@visazure.com>
2014-10-28 11:19:57 +0800

    Init

--- js/default.js
+++ js/default.js
@@ -1,1 +1,40 @@
-console.log("empty Template");
+ var windowWidth = window.innerWidth, windowHeight = window.innerHeight;
+ var camera,renderer,scene;
+ Init();
+ animate();
+
+function Init(){
+        //
+        scene = new THREE.Scene();
+  
+       //setup camera
+ 		var VIEW_ANGLE = 2.64, ASPECT = windowWidth / windowHeight, NEAR = 0.1, FAR = 10000;
+ 		camera = new LeiaCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+ 		camera.position.set(0,0,50);	
+  
+       //setup rendering parameter
+ 		renderer = new LeiaWebGLRenderer({
+         antialias:true, 
+ 		renderMode: _renderMode, 
+ 		camPanelVisible: _camPanelVisible, 
+ 		gyroPanelVisible: _gyroPanelVisible,
+ 		camFov: _camFov,    
+		devicePixelRatio: 1 
+        } );
+ 		renderer.Leia_setSize( windowWidth, windowHeight );
+ 		document.body.appendChild( renderer.domElement );
+  
+       //add object to Scene
+      var graph = new THREE.Mesh(new THREE.SphereGeometry(40, 30, 100, 100), new THREE.MeshLambertMaterial({color:0xffffff}));
+	  scene.add(graph);
+ }
+
+ function animate() 
+ {
+ 	requestAnimationFrame( animate );
+  
+    renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
+	renderer.Leia_render(scene, camera,undefined,undefined,0.4);
+ }
+
+ 
